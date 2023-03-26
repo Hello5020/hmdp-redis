8a780d3faa83c414ae0f2bd32de9aa79ac050c07
Hello <2504788150@qq.com>
2023-03-26 21:00:06 +0800

    add get voucher method

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -54,7 +54,7 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
         //逻辑过期解决缓存击穿
 //        Shop shop = queryWithLogicExpire(id);
-        Shop shop = cacheClient.queryWithPassTrough(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        Shop shop = cacheClient.queryWithLogicExpire(CACHE_SHOP_KEY, id, Shop.class, this::getById, CACHE_SHOP_TTL, TimeUnit.MINUTES);
 
         if (shop == null){
             return Result.fail("店铺不存在");
--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -1,10 +1,22 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.extension.conditions.query.QueryChainWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.SeckillVoucher;
+import com.hmdp.entity.Voucher;
 import com.hmdp.entity.VoucherOrder;
 import com.hmdp.mapper.VoucherOrderMapper;
+import com.hmdp.service.ISeckillVoucherService;
 import com.hmdp.service.IVoucherOrderService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IVoucherService;
+import com.hmdp.utils.RedisIdWorker;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -17,4 +29,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, VoucherOrder> implements IVoucherOrderService {
 
+    @Resource
+    private ISeckillVoucherService iSeckillVoucherService;
+
+    @Resource
+    RedisIdWorker redisIdWorker;
+
+    @Override
+    @Transactional
+    public Result seckillVoucher(Long voucherId) {
+        //查询优惠券
+        SeckillVoucher voucher = iSeckillVoucherService.getById(voucherId);
+        //判断是否开始
+        if (voucher.getBeginTime().isAfter(LocalDateTime.now())){
+            return Result.fail("秒杀未开始!");
+        }
+        if (voucher.getEndTime().isBefore(LocalDateTime.now())){
+            return Result.fail("秒杀已结束!");
+        }
+        //判断库存是否充足
+        boolean result = iSeckillVoucherService.update().setSql("stock = stock - 1").eq("voucher_id", voucherId).update();
+        if (!result) {
+            return Result.fail("券已售空!");
+        }
+        //创建订单
+        VoucherOrder voucherOrder = new VoucherOrder();
+        voucherOrder.setUserId(UserHolder.getUser().getId());
+        voucherOrder.setId(redisIdWorker.nextId("order"));
+        voucherOrder.setVoucherId(voucherId);
+        save(voucherOrder);
+        return Result.ok(voucherOrder.getId());
+    }
 }
